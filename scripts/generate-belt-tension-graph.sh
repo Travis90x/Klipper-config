e98508b6bae1234fdd0991a8464ac10f88a464df
Travis90x <travis_90@hotmail.it>
2023-05-27 14:26:20 +0200

    Update generate-belt-tension-graph.sh

--- scripts/generate-belt-tension-graph.sh
+++ scripts/generate-belt-tension-graph.sh
@@ -12,8 +12,8 @@ BELTX=$(ls -Art /tmp/raw_data_axis=1.000,1*.csv | tail -n 1)
 BELTY=$(ls -Art /tmp/raw_data_axis=1.000,-1*.csv | tail -n 1)
 
 rm /tmp/File_raw_data*.csv > /dev/null 2>&1
-cp "$BELTX" /tmp/File_raw_data1.csv
-cp "$BELTY" /tmp/File_raw_data2.csv
+cp "$BELTX" /tmp/Belt_X.csv
+cp "$BELTY" /tmp/Belt_Y.csv
 
 ALLCSV=/tmp/raw_data_axis*.csv
 
@@ -28,11 +28,11 @@ chown $USER:$USER ~/klipper/scripts/
 
 
 ## File renamed with date from last two CSV files
-~/klipper/scripts/graph_accelerometer.py -c /tmp/File_raw_data*.csv -o "${OUTDIR}/belts_$DATE.png"
+~/klipper/scripts/graph_accelerometer.py -c /tmp/Belt_*.csv -o "${OUTDIR}/belts_$DATE.png"
 
 ## Replace old png
 cp "${OUTDIR}/belts_$DATE.png" "${OUTDIR}/0_belts.png"
 
 ## Clean TMP folder and let you see all files from klipper
-rm /tmp/File_raw_data*.csv > /dev/null 2>&1
+rm /tmp/Belt_*.csv > /dev/null 2>&1
 mv $ALLCSV "${OUTDIR}/" > /dev/null 2>&1
